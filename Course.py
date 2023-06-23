b77bfc51d7b6ab017f50d675a32ae83448f2f30f
Matthias Riffard <matthias.riffard@avisto.com>
2023-06-23 13:51:01 +0200

    Change name to remove underscore

--- Course.py
+++ Course.py
@@ -1,11 +1,5 @@
 # hosted here: https://m-riffard-mkrank-app-vxkl35.streamlit.app/
 from __future__ import print_function
-import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 import streamlit as st
 import pandas as pd
 
